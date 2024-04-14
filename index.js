31f413db10912397720637871d7fd1e0a0e31d42
Nani7008 <narimonjon05@gmail.com>
2024-04-14 15:16:13 +0500

    60 task finished

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let xp = 0; 
+let xp = 0;
 let health = 100;
 let gold = 50;
 let currentWeapon = 0;
@@ -9,13 +9,12 @@ let inventory = ["stick"];
 const button1 = document.querySelector("#button1");
 const button2 = document.querySelector("#button2");
 const button3 = document.querySelector("#button3");
-
-const text = document.querySelector (`#text`);
+const text = document.querySelector(`#text`);
 const xpText = document.querySelector("#xpText");
 const healthText = document.querySelector("#healthText");
 const goldText = document.querySelector("#goldText");
 const monsterStats = document.querySelector("#monsterStats");
-const monsterName = document.querySelector("#monsterName"); 
+const monsterName = document.querySelector("#monsterName");
 const monsterHealthText = document.querySelector("#monsterHealth");
 
 function goTown() {
@@ -26,13 +25,13 @@ function goTown() {
     button2.onclick = goCave;
     button3.onclick = fightDragon;
     text.innerText = "You are in the town square. You see a sign that says \"Store\".";
-  }
+}
 function goStore() {
     button1.innerText = "Buy 10 health (10 gold)";
     button2.innerText = "Buy weapon (30 gold)";
     button3.innerText = "Go to town square";
     button1.onclick = buyHealth
-    button2.onclick= buyWeapon
+    button2.onclick = buyWeapon
     button3.onclick = goTown
     text.innerText = "You enter the store."
 }
@@ -45,22 +44,34 @@ function fightDragon() {
     console.log("Fighting dragon.");
 }
 
-function buyHealth () {
+function buyHealth() {
 
 }
-function buyWeapon () {
+function buyWeapon() {
 
 }
 
 function update(location) {
 
 }
+// objects inside arrays
+const locations = [{name: "town square", 
+                    "button text": ["Go to store", "Go to cave", "Fight dragon"],
+                    "button functions":[goStore, goCave, fightDragon]}];
+                   
 
-const locations =[];
 
-
-// initialize buttons
+// initialize buttons   
 
 button1.onclick = goStore
 button2.onclick = goCave
 button3.onclick = fightDragon
+
+
+
+// Objects
+
+const cat = {name:"Whiskers",
+"Number of legs": 4}
+console.log(cat);
+console.log(cat["Number of legs"]);
