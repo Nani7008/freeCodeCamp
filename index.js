fd8edcbaa15d8585479adde8acc9f3b6529e769d
Nani7008 <narimonjon05@gmail.com>
2024-04-15 21:18:09 +0500

    120

--- index.js
+++ index.js
@@ -46,6 +46,13 @@ const locations = [
         "button functions": [fightSlime, fightBeast, goTown],
         text: "You enter the cave. You see some monsters."
     },
+    {
+        name: "fight",
+        "button text": ["Attack", "Dodge", "Run",],
+        "button functions": [attack, dodge, goTown],
+        text: "You are fighting a monster."
+
+    }
 ];
 
 function goTown() {
@@ -150,6 +157,10 @@ function fightBeast() {
 function fightDragon() {
     fighting = 2;
 }
+function goFight() {
+    update (locations[3])
+    monsterHealth  = monsters[fighting].health  
+    }
 
 function attack() { };
 function dodge() { }
