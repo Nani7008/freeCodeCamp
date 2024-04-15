af212bbdf1d4cf0c98d175c10daee3227cacefbc
Nani7008 <narimonjon05@gmail.com>
2024-04-15 17:28:48 +0500

    task 99

--- index.js
+++ index.js
@@ -85,20 +85,27 @@ function buyHealth() {
         health += 10;
         goldText.innerText = gold;
         healthText.innerText = health
+
     }
     else {
         text.innerText = "You do not have enough gold to buy health."
     }
 }
 function buyWeapon() {
-    if (gold >= 30) {
-      gold -= 30;
-      currentWeapon++;
-      goldText.innerText = gold;
-      let newWeapon = weapons[currentWeapon].name;
-      text.innerText = "You now have a new weapon.";
+    if (currentWeapon < 3) {
+        if (gold >= 30) {
+            gold -= 30;
+            currentWeapon++;
+            goldText.innerText = gold;
+            let newWeapon = weapons[currentWeapon].name;
+            text.innerText = "You now have a " + newWeapon + ".";
+            inventory.push(newWeapon);
+            text.innerText += " In your inventory you have: " + inventory;
+        } else {
+            text.innerText = "You do not have enough gold to buy a weapon.";
+        }
     }
-  }
+}
 function update(location) {
     button1.innerText = location["button text"][0];
     button2.innerText = location["button text"][1];
