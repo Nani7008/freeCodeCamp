5ea1976653c1a3abb9cab8aa55f2f9e9df6bf106
Nani7008 <narimonjon05@gmail.com>
2024-04-18 10:31:57 +0500

    bug

--- index.js
+++ index.js
@@ -173,20 +173,18 @@ function goFight() {
 }
 
 function attack() {
-    text.innerText = "The " + monsters[fighting].name + " attacks."
-    text.innerText += " You attack it with your " + weapons[currentWeapon].name + "."
+    text.innerText = "The " + monsters[fighting].name + " attacks.";
+    text.innerText += " You attack it with your " + weapons[currentWeapon].name + ".";
     health -= monsters[fighting].level;
     monsterHealth -= weapons[currentWeapon].power + Math.floor(Math.random() * xp) + 1;
     healthText.innerText = health;
-    monsterHealthText.innerText = monsterHealth
+    monsterHealthText.innerText = monsterHealth;
     if (health <= 0) {
-        lose()
-
-    };
-else if (monsterHealth <= 0) {
-        defeatMonster()
+      lose();
+    } else if (monsterHealth <= 0) {
+      defeatMonster();
     }
-}
+  }
 function dodge() {
     text.innerText = "You dodge the attack from the " + monsters[fighting].name + " ."
 };
